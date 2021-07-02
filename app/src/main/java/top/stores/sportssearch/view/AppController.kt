42f163414dc532b672c849503011deb308398d49
Praveen Alyana <[alyanapraveen@gmail.com]>
2021-07-03 02:06:33 +0530

    push check2

--- app/src/main/java/top/stores/sportssearch/view/AppController.kt
+++ app/src/main/java/top/stores/sportssearch/view/AppController.kt
@@ -8,7 +8,7 @@ import android.net.NetworkInfo
 class AppController : Application() {
 
     init {
-        // App Instance test 3r3w
+        // App Instance test 3r3wtrt
         instance = this
     }
 
